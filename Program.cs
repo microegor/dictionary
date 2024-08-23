a6aabcec48924637aad3f9b299e9c7cb74f897b2
microegor <microegor321@gmail.com>
2024-08-23 12:24:58 +0200

    feat: теперь слова идут в случайном порядке

--- Program.cs
+++ Program.cs
@@ -17,7 +17,11 @@ class Program
         Console.WriteLine("Выберите язык (1 - русский, 2 - английский):");
         int fromLanguage = int.Parse(Console.ReadLine());
 
-        foreach (var wordPair in reader.Words)
+        // Перемешать список слов
+        Random random = new Random();
+        var shuffledWords = reader.Words.OrderBy(x => random.Next()).ToList();
+
+        foreach (var wordPair in shuffledWords)
         {
             string wordToTranslate = fromLanguage == 1 ? wordPair.Value : wordPair.Key;
             Console.WriteLine("Слово: " + wordToTranslate);
@@ -25,6 +29,7 @@ class Program
             // Запросить у пользователя перевод слова
             Console.Write("Введите перевод слова " + wordToTranslate + ": ");
             string translation = Console.ReadLine();
+
             // Отладочная информация
             Console.WriteLine("Translation entered: " + translation);
             Console.WriteLine("Translation length: " + translation.Length);
@@ -47,6 +52,26 @@ class Program
         }
 
         Console.WriteLine("Все слова пройдены.");
+    }
+
+    static string ReadLineWithEncoding()
+    {
+        byte[] inputBytes = Console.OpenStandardInput().ReadBytes();
+        return Encoding.UTF8.GetString(inputBytes).TrimEnd('\r', '\n');
+    }
+}
+
+static class StreamExtensions
+{
+    public static byte[] ReadBytes(this System.IO.Stream stream)
+    {
+        List<byte> bytes = new List<byte>();
+        int b;
+        while ((b = stream.ReadByte()) != -1)
+        {
+            bytes.Add((byte)b);
+        }
+        return bytes.ToArray();
     }
 }
 
