1047e92abe1a89e707ac081f7243300eb7ea0f23
microegor <microegor321@gmail.com>
2024-08-23 12:39:55 +0200

    fix: исправлена ошибка с русским языком

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ class Program
     {
         // Установить кодировку консоли на UTF-8
         Console.OutputEncoding = Encoding.UTF8;
-        Console.InputEncoding = Encoding.UTF8;
+        Console.InputEncoding = System.Text.Encoding.GetEncoding("utf-16");
 
         DictionaryReader reader = new DictionaryReader("dictionary.xml");
 
@@ -30,14 +30,6 @@ class Program
             Console.Write("Введите перевод слова " + wordToTranslate + ": ");
             string translation = Console.ReadLine();
 
-            // Отладочная информация
-            Console.WriteLine("Translation entered: " + translation);
-            Console.WriteLine("Translation length: " + translation.Length);
-
-            // write translation to console in hex
-            byte[] translationBytes = Encoding.UTF8.GetBytes(translation);
-            Console.WriteLine("Translation in hex: " + BitConverter.ToString(translationBytes).Replace("-", " "));
-
             // Проверить правильность перевода
             string correctTranslation = fromLanguage == 1 ? wordPair.Key : wordPair.Value;
 
@@ -52,26 +44,6 @@ class Program
         }
 
         Console.WriteLine("Все слова пройдены.");
-    }
-
-    static string ReadLineWithEncoding()
-    {
-        byte[] inputBytes = Console.OpenStandardInput().ReadBytes();
-        return Encoding.UTF8.GetString(inputBytes).TrimEnd('\r', '\n');
-    }
-}
-
-static class StreamExtensions
-{
-    public static byte[] ReadBytes(this System.IO.Stream stream)
-    {
-        List<byte> bytes = new List<byte>();
-        int b;
-        while ((b = stream.ReadByte()) != -1)
-        {
-            bytes.Add((byte)b);
-        }
-        return bytes.ToArray();
     }
 }
 
